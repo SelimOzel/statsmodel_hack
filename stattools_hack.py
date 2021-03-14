a1de9d7a2e80a9fac4335cdc363c9a866ea878e6
Selim Ozel <sozel@wpi.edu>
2021-03-14 14:11:58 +0100

    Lagmat function added. Removed ispandas because it was soooooooooooooo long.

--- stattools_hack.py
+++ stattools_hack.py
@@ -235,6 +235,135 @@ def adfuller(
         else:
             return adfstat, pvalue, usedlag, nobs, critvalues, icbest
 
+def lagmat(x, maxlag, trim='forward', original='ex', use_pandas=False):
+    """
+    Create 2d array of lags.
+
+    Parameters
+    ----------
+    x : array_like
+        Data; if 2d, observation in rows and variables in columns.
+    maxlag : int
+        All lags from zero to maxlag are included.
+    trim : {'forward', 'backward', 'both', 'none', None}
+        The trimming method to use.
+
+        * 'forward' : trim invalid observations in front.
+        * 'backward' : trim invalid initial observations.
+        * 'both' : trim invalid observations on both sides.
+        * 'none', None : no trimming of observations.
+    original : {'ex','sep','in'}
+        How the original is treated.
+
+        * 'ex' : drops the original array returning only the lagged values.
+        * 'in' : returns the original array and the lagged values as a single
+          array.
+        * 'sep' : returns a tuple (original array, lagged values). The original
+                  array is truncated to have the same number of rows as
+                  the returned lagmat.
+    use_pandas : bool
+        If true, returns a DataFrame when the input is a pandas
+        Series or DataFrame.  If false, return numpy ndarrays.
+
+    Returns
+    -------
+    lagmat : ndarray
+        The array with lagged observations.
+    y : ndarray, optional
+        Only returned if original == 'sep'.
+
+    Notes
+    -----
+    When using a pandas DataFrame or Series with use_pandas=True, trim can only
+    be 'forward' or 'both' since it is not possible to consistently extend
+    index values.
+
+    Examples
+    --------
+    >>> from statsmodels.tsa.tsatools import lagmat
+    >>> import numpy as np
+    >>> X = np.arange(1,7).reshape(-1,2)
+    >>> lagmat(X, maxlag=2, trim="forward", original='in')
+    array([[ 1.,  2.,  0.,  0.,  0.,  0.],
+       [ 3.,  4.,  1.,  2.,  0.,  0.],
+       [ 5.,  6.,  3.,  4.,  1.,  2.]])
+
+    >>> lagmat(X, maxlag=2, trim="backward", original='in')
+    array([[ 5.,  6.,  3.,  4.,  1.,  2.],
+       [ 0.,  0.,  5.,  6.,  3.,  4.],
+       [ 0.,  0.,  0.,  0.,  5.,  6.]])
+
+    >>> lagmat(X, maxlag=2, trim="both", original='in')
+    array([[ 5.,  6.,  3.,  4.,  1.,  2.]])
+
+    >>> lagmat(X, maxlag=2, trim="none", original='in')
+    array([[ 1.,  2.,  0.,  0.,  0.,  0.],
+       [ 3.,  4.,  1.,  2.,  0.,  0.],
+       [ 5.,  6.,  3.,  4.,  1.,  2.],
+       [ 0.,  0.,  5.,  6.,  3.,  4.],
+       [ 0.,  0.,  0.,  0.,  5.,  6.]])
+    """
+    maxlag = int_like(maxlag, 'maxlag')
+    use_pandas = bool_like(use_pandas, 'use_pandas')
+    trim = string_like(trim, 'trim', optional=True,
+                       options=('forward', 'backward', 'both', 'none'))
+    original = string_like(original, 'original', options=('ex', 'sep', 'in'))
+
+    # TODO:  allow list of lags additional to maxlag
+    orig = x
+    x = array_like(x, 'x', ndim=2, dtype=None)
+    #is_pandas = _is_using_pandas(orig, None) and use_pandas
+    trim = 'none' if trim is None else trim
+    trim = trim.lower()
+    #if is_pandas and trim in ('none', 'backward'):
+    #    raise ValueError("trim cannot be 'none' or 'forward' when used on "
+    #                     "Series or DataFrames")
+
+    dropidx = 0
+    nobs, nvar = x.shape
+    if original in ['ex', 'sep']:
+        dropidx = nvar
+    if maxlag >= nobs:
+        raise ValueError("maxlag should be < nobs")
+    lm = np.zeros((nobs + maxlag, nvar * (maxlag + 1)))
+    for k in range(0, int(maxlag + 1)):
+        lm[maxlag - k:nobs + maxlag - k,
+        nvar * (maxlag - k):nvar * (maxlag - k + 1)] = x
+
+    if trim in ('none', 'forward'):
+        startobs = 0
+    elif trim in ('backward', 'both'):
+        startobs = maxlag
+    else:
+        raise ValueError('trim option not valid')
+
+    if trim in ('none', 'backward'):
+        stopobs = len(lm)
+    else:
+        stopobs = nobs
+    '''
+    if is_pandas:
+        x = orig
+        x_columns = x.columns if isinstance(x, DataFrame) else [x.name]
+        columns = [str(col) for col in x_columns]
+        for lag in range(maxlag):
+            lag_str = str(lag + 1)
+            columns.extend([str(col) + '.L.' + lag_str for col in x_columns])
+        lm = DataFrame(lm[:stopobs], index=x.index, columns=columns)
+        lags = lm.iloc[startobs:]
+        if original in ('sep', 'ex'):
+            leads = lags[x_columns]
+            lags = lags.drop(x_columns, 1)
+    '''
+    #else:
+    lags = lm[startobs:stopobs, dropidx:]
+    if original == 'sep':
+        leads = lm[startobs:stopobs, :dropidx]
+
+    if original == 'sep':
+        return lags, leads
+    else:
+        return lags
 
 def int_like(
     value: Any, name: str, optional: bool = False, strict: bool = False
@@ -403,6 +532,7 @@ def array_like(
         arr = np.ascontiguousarray(arr, dtype=dtype)
     return arr
 
+# Obtained from statsmodels.tools.validation
 def string_like(value, name, optional=False, options=None, lower=True):
     """
     Check if object is string-like and raise if not
@@ -448,6 +578,7 @@ def string_like(value, name, optional=False, options=None, lower=True):
         raise ValueError(msg)
     return value
 
+# Obtained from statsmodels.tools.validation
 def bool_like(value, name, optional=False, strict=False):
     """
     Convert to bool or raise if not bool_like
@@ -487,3 +618,8 @@ def bool_like(value, name, optional=False, strict=False):
             "{0} must be a bool (or bool-compatible)"
             "{1}".format(name, extra_text)
         )
+
+# Obtained from statsmodels.tools.data
+#def _is_using_pandas(endog, exog):
+#    from statsmodels.compat.pandas import data_klasses as klasses
+#    return (isinstance(endog, klasses) or isinstance(exog, klasses))
